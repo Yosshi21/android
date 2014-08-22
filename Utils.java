64eea18a3e855d7fd71854e8088db838d81a0f2e
Yosshi21 <wad.863.xx@gmail.com>
2014-08-22 19:03:04 +0900

    Create Utils.java
    Util関連をまとめたクラス

---
+++ Utils.java
@@ -0,0 +1,43 @@
+/**
+ * Utilsクラス
+ *
+ */
+public class Utils {
+
+	/**
+	 *  getText風に文字列を取得。
+	 *  getResources().getString(R.string.someString) の
+	 *  シンタックスシュガー
+	 */
+	public static String getText(String target_string_name,
+		Activity activity_instance) {
+		// リソースIDを取得
+		int target_string_id = activity_instance
+			.getResources()
+			.getIdentifier(
+				target_string_name,
+				"string",
+				activity_instance.getPackageName()
+			);
+
+		// リソースから取得
+		String target_string = getText(target_string_id, 
+			activity_instance);
+
+		return target_string;
+	}
+
+	/**
+	 * 上記メソッドを型セーフにしたもの
+	 */
+	public static String getText(int target_string_id, 
+		Activity activity_instance) {
+		// リソースから取得
+		String target_string = activity_instance
+			.getResources()
+			.getString( target_string_id )
+		;
+
+		return target_string;
+	}
+}
