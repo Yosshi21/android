3e30cbe15275776e1ecd9f7cb4ebdc8b96f86b20
Yosshi21 <wad.863.xx@gmail.com>
2015-03-04 19:38:45 +0900

    Update BaseActivity.java

--- BaseActivity.java
+++ BaseActivity.java
@@ -7,13 +7,13 @@ public abstract class BaseActivity extends Activity {
 	 *  文字列を取得
 	 */
 	public String _(String target_string_name) {
-		return CommonUtil.getText(target_string_name, this);
+		return StrUtils.getText(target_string_name, this);
 	}
 
 	/**
 	 *  getTextの型セーフ版
 	 */
 	public String _(int target_string_id) {
-		return CommonUtil.getText(target_string_id, this);
+		return StrUtils.getText(target_string_id, this);
 	}
 }
